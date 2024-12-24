50c5950789a9a07cfb2a89dbaf537dcee814585c
gokul3710 <gokulkrishnawaryar@gmail.com>
2024-12-24 19:06:59 +0530

    feat: Added jwt auth

--- server/src/jobs/jobs.controller.ts
+++ server/src/jobs/jobs.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Body, Req, Headers } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Body, Req, Headers, UseGuards } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JobsService } from './jobs.service';
 import { CreatePrintJobDto } from './dto/create-print-job.dto';
+import { UserGuard } from 'src/common/guards/user.guard';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('jobs')
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Post('submit')
+  @UseGuards(UserGuard)
   @UseInterceptors(FileInterceptor('file'))
   async submitJob(
+    @User() userId: string,
     @UploadedFile() file: Express.Multer.File,
     @Body() body: CreatePrintJobDto,
     @Headers() headers: { [key: string]: string },
@@ -20,6 +24,7 @@ export class JobsController {
       settings: body.settings,
       printerName: body.printerName,
       clientId: headers['x-client-id'],
+      userId: userId
     };
     return this.jobsService.submitJob(job);
   }
--- server/src/jobs/jobs.module.ts
+++ server/src/jobs/jobs.module.ts
@@ -4,12 +4,14 @@ import { JobsService } from './jobs.service';
 import { JobsConsumerService } from './jobs.consumer';
 import { GatewayModule } from '../gateway/gateway.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   imports: [GatewayModule,
     MulterModule.register({
       dest: './uploads', // Directory to save uploaded files
     }),
+    CommonModule
   ],
   controllers: [JobsController],
   providers: [JobsService, JobsConsumerService],
